990a946d7653577ca7c3b2f0634a03c1ea42b74e
adekola <adekola.m.adebayo@gmail.com>
2016-03-02 22:30:06 +0100

    Demo'd the use of TypedArrays in ES6

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var buf = new Buffer('Hello', 'utf8'); 
-console.log(buf); //outputs in hx format
-console.log(buf.toString());
-console.log(buf.toJSON());
-console.log(buf[2]);
-buf.write('wo');
-console.log(buf.toString());
+//ES6 typed arrays
+var buffer = new ArrayBuffer(8); //Binary data. 8 bytes ==> 64bits. This is directly available in ES6
+var view = new Int32Array(buffer);//used to look into the buffer. The type of the array determines how the buffer contents will be made available
+view[0] = 5;
+view[1] = 15;
+view[2] = 33; //this doesn't get stored. by definition, the arraybuffer can hold up to 64bits of data. i.e. 2 integers (32 bits each)
+console.log(view);
