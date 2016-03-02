e874b383a64558a19710338c98ad38eb73236586
adekola <adekola.m.adebayo@gmail.com>
2016-03-02 22:38:50 +0100

    Demo'd the use of Callbacks

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-//ES6 typed arrays
-var buffer = new ArrayBuffer(8); //Binary data. 8 bytes ==> 64bits. This is directly available in ES6
-var view = new Int32Array(buffer);//used to look into the buffer. The type of the array determines how the buffer contents will be made available
-view[0] = 5;
-view[1] = 15;
-view[2] = 33; //this doesn't get stored. by definition, the arraybuffer can hold up to 64bits of data. i.e. 2 integers (32 bits each)
-console.log(view);
+function greet(callback){ //expects a callback fxn to be called from within the greet fxn
+    console.log('Hello!');
+    var data = {
+      name: 'John Doe'  
+    };
+    callback(data);
+}
+
+greet(function(data) {
+    console.log('The callback was invoked');
+    console.log(data);
+    
+});
