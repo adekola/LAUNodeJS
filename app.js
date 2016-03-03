3afddc5b902313e14eb8e32afb2921ead31b4f79
adekola <adekola.m.adebayo@gmail.com>
2016-03-03 21:02:18 +0100

    Demo'd the use of body and json parser middlewares

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use('/assets',express.static(__dirname + '/public'));
 
+app.set('view engine', 'ejs');
+
 var port =  process.env.PORT || 3000;
 
+var urlEncodedParser = bodyParser.urlencoded({ extended: false});
+
+var jsonParser = bodyParser.json();
+
 app.get('/', function (req, res, next) {
-    console.log('Request URL: ' +  req.url);
-    res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet/></head><body> <h1> Hello my love </h1> </body></html>');
-    next();
+    res.render('index');
 });
 
 
 app.get('/person/:id', function(req, res){
-    res.send('<html><head></head><body> <h1> Person: '+ req.params.id + ' </h1> </body></html>');
+    res.render('person', {ID: req.params.id, Qstr: req.query.qstr});    
+});
+
+//middleware body parser to be called before the request is processed
+app.post('/person/', urlEncodedParser, function(req, res){
+    res.send('Thank You');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+});
+
+app.post('/personjson/', jsonParser, function(req, res){
+    res.send('Thank You for the json data');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
 });
 
 app.get('/api', function(req, res){
