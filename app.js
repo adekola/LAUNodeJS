64db2717c86c4107688e7320ea8eb22c8848976d
adekola <adekola.m.adebayo@gmail.com>
2016-03-03 18:19:40 +0100

    Demo'd a basic impl of routing

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function(req, res){
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    var obj = {
-        firstname: 'John',  
-        lastname: 'Doe'
-    }   
-    res.end(JSON.stringify(obj));
+http.createServer(function(req, res){ //primitive routing capabilities
+    if(req.url === '/'){
+        fs.createReadStream(__dirname + '/index.html').pipe(res);    
+    }
+    
+    if(req.url === '/api'){
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        var obj = {
+            firstname: 'John',  
+            lastname: 'Doe'
+        }   
+        res.end(JSON.stringify(obj));
+    }
+    else{
+        res.writeHead(404);
+        res.end("Don't know what you're talking about");
+    }
+    
 }).listen(1337, '127.0.0.1');
