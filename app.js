3433deef5afa3cd6e87f4c3c484a66ed317f2ab5
adekola <adekola.m.adebayo@gmail.com>
2016-03-03 12:25:34 +0100

    Demo'd the canonical Hello World web server example

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
-var fs = require('fs');
-var zlib = require('zlib');
+var http = require('http');
 
-var readable = fs.createReadStream(__dirname + '/greet.txt');
-
-var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
-
-var compressed  = fs.createWriteStream(__dirname + '/greet.txt.gz');
-
-var gzip = zlib.createGzip(); //this is a transfrom stream, which compresses each chunk sent to it
-
-//returns the destination stream that's passed in as the parameter
-readable.pipe(writable).pipe; //a nifty and neat way to copy data from one stream to the other
-
-//read, pipe to gzip (for compression), then pipe to the compressed stream for saving to file
-readable.pipe(gzip).pipe(compressed);
+http.createServer(function(req, res){
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n'); //this is the last thing i'm sending
+}).listen(1337, '127.0.0.1');
