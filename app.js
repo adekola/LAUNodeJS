6aeb4200f7ec2fa287e7e5c661a1650695d2f7b3
adekola <adekola.m.adebayo@gmail.com>
2016-03-03 18:42:50 +0100

    Demo'd use of an npm package

--- app.js
+++ app.js
@@ -1,22 +1,2 @@
-var http = require('http');
-var fs = require('fs');
-
-http.createServer(function(req, res){ //primitive routing capabilities
-    if(req.url === '/'){
-        fs.createReadStream(__dirname + '/index.html').pipe(res);    
-    }
-    
-    if(req.url === '/api'){
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        var obj = {
-            firstname: 'John',  
-            lastname: 'Doe'
-        }   
-        res.end(JSON.stringify(obj));
-    }
-    else{
-        res.writeHead(404);
-        res.end("Don't know what you're talking about");
-    }
-    
-}).listen(1337, '127.0.0.1');
+var moment = require('moment');
+console.log(moment().format("ddd, hA"));
