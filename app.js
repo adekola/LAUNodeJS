f896aa38f66e81a8ca11764fc074e5493f1bc1e7
adekola <adekola.m.adebayo@gmail.com>
2016-03-03 10:46:38 +0100

    Demo'd the use of Streams

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-function greet(callback){ //expects a callback fxn to be called from within the greet fxn
-    console.log('Hello!');
-    var data = {
-      name: 'John Doe'  
-    };
-    callback(data);
-}
+var fs = require('fs');
 
-greet(function(data) {
-    console.log('The callback was invoked');
-    console.log(data);
-    
-});
+var readable = fs.createReadStream(__dirname + '/greet.txt', { encoding : 'utf8', highWaterMark: 16 * 1024}); //highwatermark specifies the size of the buffer
+
+var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
+
+
+readable.on('data', function(chunk) { //listen for the data event and write each chunk progressively to the other blank file
+    console.log(chunk.length);
+    writable.write(chunk);
+})
