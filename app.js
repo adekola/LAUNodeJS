c57da7f64d50693f56f977f24bad07052085a332
adekola <adekola.m.adebayo@gmail.com>
2016-03-03 11:15:26 +0100

    Demo'd the use of Pipes

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var fs = require('fs');
+var zlib = require('zlib');
 
-var readable = fs.createReadStream(__dirname + '/greet.txt', { encoding : 'utf8', highWaterMark: 16 * 1024}); //highwatermark specifies the size of the buffer
+var readable = fs.createReadStream(__dirname + '/greet.txt');
 
 var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
 
+var compressed  = fs.createWriteStream(__dirname + '/greet.txt.gz');
 
-readable.on('data', function(chunk) { //listen for the data event and write each chunk progressively to the other blank file
-    console.log(chunk.length);
-    writable.write(chunk);
-})
+var gzip = zlib.createGzip(); //this is a transfrom stream, which compresses each chunk sent to it
+
+//returns the destination stream that's passed in as the parameter
+readable.pipe(writable).pipe; //a nifty and neat way to copy data from one stream to the other
+
+//read, pipe to gzip (for compression), then pipe to the compressed stream for saving to file
+readable.pipe(gzip).pipe(compressed);
