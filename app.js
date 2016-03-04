bf5bd947a6cb7b17ab50c1befb88f9e4edc5d282
adekola <adekola.m.adebayo@gmail.com>
2016-03-04 08:00:17 +0100

    Demo'd the use of mongoose and mongolab for data persistence

--- app.js
+++ app.js
@@ -1,40 +1,66 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://test:test@ds019468.mlab.com:19468/addressbook');
+
+var Schema = mongoose.Schema;
+
+var personSchema = new Schema({
+    firstname: String, 
+    lastname: String,
+    address: String
+});
+
+//function ctor to generate objects based on the schema defined earlier
+var Person = mongoose.model('Person', personSchema);
+var john  = Person({
+    firstname: 'John',
+    lastname: 'Doe',
+    address: '555 Main St.'
+});
+
+//save the user
+john.save(function(err) {
+    if(err) throw err;
+    console.log('person saved!');
+});
+
+var jane  = Person({
+    firstname: 'Jane',
+    lastname: 'Doe',
+    address: '555 Main St.'
+});
+
+//save the user
+jane.save(function(err) {
+    if(err) throw err;
+    console.log('person saved!');
+});
+
+var  apiController = require('./controllers/apiController');
+var  htmlController = require('./controllers/htmlController');
+
+var port =  process.env.PORT || 3000;
 
 app.use('/assets',express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
-var port =  process.env.PORT || 3000;
 
-var urlEncodedParser = bodyParser.urlencoded({ extended: false});
+app.use('/', function (req, res, next) {
+    console.log('Request URL: ' + req.url);
+    
+    //get all users
+    Person.find({}, function(err, users) {
+        if(err) throw err;
+        console.log(users);
+    })
+    next();
+})
 
-var jsonParser = bodyParser.json();
+htmlController(app);
 
-app.get('/', function (req, res, next) {
-    res.render('index');
-});
+apiController(app);
 
-
-app.get('/person/:id', function(req, res){
-    res.render('person', {ID: req.params.id, Qstr: req.query.qstr});    
-});
-
-//middleware body parser to be called before the request is processed
-app.post('/person/', urlEncodedParser, function(req, res){
-    res.send('Thank You');
-    console.log(req.body.firstname);
-    console.log(req.body.lastname);
-});
-
-app.post('/personjson/', jsonParser, function(req, res){
-    res.send('Thank You for the json data');
-    console.log(req.body.firstname);
-    console.log(req.body.lastname);
-});
-
-app.get('/api', function(req, res){
-    res.json({ firstname: 'Kola', lastname: 'Ade'});
-});
 app.listen(port);
